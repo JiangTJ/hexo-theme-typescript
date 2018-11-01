051e3f042e18306331c68bc89941c5620febf4e6
jiangtj <116749895@qq.com>
2018-11-01 13:06:43 +0800

    fix url

--- source/js/search.js
+++ source/js/search.js
@@ -7,7 +7,7 @@ var SearchService = "";
    */
 
    // if search.enable is false, not init.
-   if ($(".search")) {
+   if (!HexoConfig.search.enabled || HexoConfig.search.enabled === "false") {
      return;
    }
 
@@ -201,7 +201,7 @@ var SearchService = "";
       var html = "";
       if (self.config.brands[service] && self.config.brands[service].logo) {
         html += "<a href='" +self.config.brands[service].url+ "' class='" +service+ "'>";
-        html +=    '<img src="' +self.config.imagePath+self.config.brands[service].logo+ '" />';
+        html +=    '<img src="' + HexoConfig.root.substr(0,HexoConfig.root.length-1) + self.config.imagePath + self.config.brands[service].logo + '" />';
         html += "</a>";
         self.dom.modal_logo.html(html);
       }
